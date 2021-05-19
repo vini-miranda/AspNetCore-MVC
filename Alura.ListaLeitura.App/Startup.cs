ef5f0f0e7612faef88961f11f8dab50db36f8f99
Vinicius Miranda <v-alves-miranda@live.com>
2021-05-19 15:14:46 -0300

    Iniciando estudos sobre asp net mvc

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
+            {
+                { "/Livros/Paraler", LivrosParaLer },
+                { "/Livros/Lendo", LivrosLendo },
+                { "/Livros/Lidos", LivrosLidos }
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                var metodo = caminhosAtendidos[context.Request.Path];
+                return metodo.Invoke(context);
+            }
+
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("Rota Inválida");
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
+        }
+    }
+}
